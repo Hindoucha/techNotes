53aa40a0ffbf8d1dac61e4b736f1bd2bd11019a7
Hind BENSAFI <hindbensafi@gmail.com>
2022-10-16 15:51:56 +0100

    chapter 2: Middleware

---
+++ middlewares/errHandler.js
@@ -0,0 +1,12 @@
+const {logEvents}= require('../middlewares/logger')
+
+const errHandler = (err, req, res, next) => {
+    logEvents(`${err.name}\t${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLogs.log')
+    console.log(`${err.stack}`)
+
+    const status = err.status ? err.status : 500 // server error
+
+    res.json({message: err.message})
+}
+
+module.exports = errHandler
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+const {logger} = require('./middlewares/logger')
+const corsOptions = require('./config/corsOptions')
+const errHandler = require('./middlewares/errHandler')
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
-// telling server where to find static files
-app.use('/', express.static(path.join(__dirname, '/public')))
 
+// MIDDLEWARES
+// custum
+app.use(logger)
+// built-in
+app.use(express.json())
+// built-in : telling server where to find static files
+app.use('/', express.static(path.join(__dirname, 'public')))
+// 3rd party
+app.use(cors(corsOptions))
+app.use(cookieParser())
+
+
+// ROUTES
 // where to find other normal routes
 app.use('/', require('./routes/root'))
 
@@ -21,6 +38,9 @@ app.all('*', (req, res)=> {
         res.type('.text').send('404 Not found')
     }
 })
+
+// err handler middleware
+app.use(errHandler)
 
 app.listen(PORT, ()=>{
     console.log(`Server listenning on port ${PORT}`)
